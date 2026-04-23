22066e128586ce085b04a571b1178940b7b5e6d4
BlackBaroness <57033600+BlackBaroness@users.noreply.github.com>
2026-04-23 05:15:35 +0300

    fix publishing

--- buildSrc/src/main/kotlin/buildlogic.publish.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.publish.gradle.kts
@@ -2,3 +2,17 @@ plugins {
   `java-library`
   `maven-publish`
 }
+
+java {
+  withJavadocJar()
+  withSourcesJar()
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+    }
+  }
+}
+
